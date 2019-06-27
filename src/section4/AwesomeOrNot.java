9d66fcfd33d8f8d8e7d13bf8c37497c716459e35
League Student <league@iMac.attlocal.net>
2019-06-27 14:25:26 -0700

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,31 +2,36 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int randomNumber = new Random().nextInt(3);
 	// 3. Print out this variable
-
+System.out.println(randomNumber);
 	// 4. Get the user to enter something that they think is awesome
-
+JOptionPane.showInputDialog("Name one thing you think is awesome");
 	// 5. If the random number is 0
-
+JOptionPane.showMessageDialog(null,"If your number was 0");
 	// -- tell the user whatever they entered is awesome!
-
+JOptionPane.showMessageDialog(null,"Your entered answer is awesome!");
 	// 6. If the random number is 1
-
+JOptionPane.showMessageDialog(null, "If your number was 1");
 	// -- tell the user whatever they entered is ok.
-
+JOptionPane.showMessageDialog(null, "Your entered answer is ok");
 	// 7. If the random number is 2
-
+JOptionPane.showMessageDialog(null, "If your number was 2");
 	// -- tell the user whatever they entered is boring.
-
+JOptionPane.showMessageDialog(null, "Whatever you entered is boring");
 	// 8. If the random number is 3
-
+JOptionPane.showMessageDialog(null,"If your random number was 3");
 	// -- write your own answer
+JOptionPane.showMessageDialog(null,"Everybody likes the thing you entered");
+}
 
 }
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,47 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 8;
 		// 2.  Ask the user a question 
-		
+		String answer = JOptionPane.showInputDialog("Do you play sports");
+		if(answer.equals("no")){
+			System.out.println("Ew you have no life");
+		}
 		// 3.  Use an if statement to check if their answer is correct
 		
 		// 4.  if the user's answer was correct, add one to their score 
-		
+		if (answer.equals("yes")) {
+			System.out.println("good for you");
+			
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String question = JOptionPane.showInputDialog("do you play games?");
+		if(answer.equals("no")){
+			System.out.println("ew");
+		}
+		if(answer.equals("Yes")) { 
+			System.out.println("Lets play together hahaha");
+		}	
 		
+		
+		
+		
+	
+		
+		
+	
+			
 		// 6.  After all the questions have been asked, print the user's score 
-		
-	}
+	
+		}
+	
+	
+	
+
 }
